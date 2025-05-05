25e0d86744b0f3a0aa2124eec8e591943d6ab74b
Heloiisee <Heloiseguicheron71@gmail.com>
2025-05-05 19:40:17 -0400

    Modif

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+import './bootstrap';
+import '../sass/styles.scss';
+import '../js/intersectionObserver.js';
+import '../js/script.js';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -8,7 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Portfolio')</title>
 
-    @vite(['resources/sass/styles.scss', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
 
     <!-- FontAwesome -->
